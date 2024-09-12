c98e6c30b1bfa7ace8f260c4bae48dec1e75a5e0
yinpeng <2291314224@qq.com>
2024-09-12 20:58:18 +0800

    优化: 添加Claude模型支持并改进API请求处理

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-# 设置日志级别from flask import Flask, request, Response, stream_with_context, make_response
 import random
-from flask import Flask, Response, make_response, request, stream_with_context
+from flask import Flask, Response, jsonify, make_response, request, stream_with_context
 import requests
 import logging
 import os
@@ -16,6 +15,7 @@ logging.basicConfig(level=logging.INFO)
 # 从.env文件中获取API_KEYS和API_URLS
 API_KEYS = os.getenv('API_KEYS', '').split(',')
 API_URLS = os.getenv('API_URLS', '').split(',')
+CLAUDE_API_URLS = os.getenv('CLAUDE_API_URLS', '').split(',')
 
 SECRET_TOKEN = os.getenv('SECRET_TOKEN')
 BARK_URL = os.getenv('BARK_URL')
@@ -40,6 +40,20 @@ def proxy(path):
     # 检查请求路径
     if f"/{path}" not in ALLOWED_PATHS:
         return Response("Not Found", status=404)
+    
+    if path == "v1/models":
+        # 处理 /v1/models 请求
+        models_data = {
+            "data": [
+                {"id": "gpt-4o"},
+                {"id": "gpt-4o-mini"},
+                {"id": "claude-3-5-sonnet-20240620"},
+                {"id": "claude-3-opus-20240229"},
+                {"id": "claude-3-sonnet-20240229"},
+                {"id": "claude-3-haiku-20240307"},
+            ]
+        }
+        return jsonify(models_data), 200
 
     # 验证incoming请求
     auth_header = request.headers.get("Authorization")
@@ -49,14 +63,21 @@ def proxy(path):
         or auth_header.split(" ")[1] != SECRET_TOKEN
     ):
         return Response("Unauthorized", status=401)
+    
+    # 获取请求体
+    request_data = request.json
 
+    # 获取model_name
+    model_name = request_data.get('model')
+    
     # 随机选择API密钥和URL
-    random_key = random.choice(API_KEYS)
-    random_url = random.choice(API_URLS)
+    random_key = random.choice(API_KEYS) if "gpt" in model_name else ""
+    random_url = random.choice(API_URLS) if "gpt" in model_name else random.choice(CLAUDE_API_URLS)
 
     # 创建新的请求头
     headers = dict(request.headers)
-    headers["Authorization"] = f"Bearer {random_key}"
+    if "gpt" in model_name:
+        headers["Authorization"] = f"Bearer {random_key}"
     headers["Host"] = f"{random_url.split('//')[1]}"
     # 构建目标URL
     target_url = f"{random_url}/{path}"
@@ -64,49 +85,33 @@ def proxy(path):
     # 打印必要的URL
     logging.info(f"请求方法: {request.method}")
     logging.info(f"目标URL: {target_url}")
+    logging.info(f"mode_name: {model_name}")
     logging.info(f"random_key: {random_key}")
-
-    # 发送请求到目标API
-    if request.method == "POST":
-        # 获取请求体
-        request_data = request.json
+    
+    # 检查是否为流式请求
+    is_stream = request_data.get('stream', False)
+    
+    if is_stream:
+        # 流式请求
+        response = requests.post(target_url, headers=headers, json=request_data, stream=True)
         
-        # 检查是否为流式请求
-        is_stream = request_data.get('stream', False)
-        
-        if is_stream:
-            # 流式请求
-            response = requests.post(target_url, headers=headers, json=request_data, stream=True)
-            
-            def generate():
-                for chunk in response.iter_lines():
-                    if chunk:
-                        yield f"{chunk.decode('utf-8')}\n\n"
-            if response.status_code != 200:
-                logging.warning(f"响应状态码: {response.status_code}")
-                logging.warning(f"响应内容: {response.text}")
-                
-                message = f"响应状态码: {response.status_code}\n响应内容: {response.text}\n请求方法: {request.method}\n目标URL: {random_url[7:]}\nrandom_key: {random_key}"
-                encoded_message = urllib.parse.quote(message)
-                request_url = f"{BARK_URL}chat2api-balance/{encoded_message}"
-                requests.get(request_url, timeout=5)
-            return Response(stream_with_context(generate()), content_type='text/event-stream')
-        else:
-            # 非流式请求
-            response = requests.post(target_url, headers=headers, json=request_data)
-            
-            # 打印响应状态码
-            logging.info(f"响应状态码: {response.status_code}")
-            
-            response = Response(
-                response.content, status=response.status_code, headers=dict(response.headers)
-            )
-    else:  # GET
-        response = requests.get(target_url, headers=headers)
-        
+        def generate():
+            for chunk in response.iter_lines():
+                if chunk:
+                    yield f"{chunk.decode('utf-8')}\n\n"
+        if response.status_code != 200:
+            logging.warning(f"响应状态码: {response.status_code}")
+            logging.warning(f"响应内容: {response.text}")
+            message = f"响应状态码: {response.status_code}\n响应内容: {response.text}\n请求方法: {request.method}\n目标URL: {random_url[7:]}\nmodel_name: {model_name}\nrandom_key: {random_key}"
+            encoded_message = urllib.parse.quote(message)
+            request_url = f"{BARK_URL}chat2api-balance/{encoded_message}"
+            requests.get(request_url, timeout=5)
+        return Response(stream_with_context(generate()), content_type='text/event-stream')
+    else:
+        # 非流式请求
+        response = requests.post(target_url, headers=headers, json=request_data)
         # 打印响应状态码
         logging.info(f"响应状态码: {response.status_code}")
-        
         response = Response(
             response.content, status=response.status_code, headers=dict(response.headers)
         )
@@ -115,7 +120,6 @@ def proxy(path):
     response.headers.add("Access-Control-Allow-Origin", "*")
     response.headers.add("Access-Control-Allow-Headers", "*")
     response.headers.add("Access-Control-Allow-Methods", "*")
-    
     return response
 
 if __name__ == "__main__":
